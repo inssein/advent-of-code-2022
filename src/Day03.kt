29bf4c9c4685752be6d1f4e1171722febf0116a6
Hussein Jafferjee <hussein@jafferjee.ca>
2022-12-03 09:24:00 -0800

    day3 - implementation of part1 and part2

---
+++ src/Day03.kt
@@ -0,0 +1,39 @@
+fun main() {
+    fun findCommon(a: String, b: String): String {
+        val set = a.toSet()
+
+        return b.filter { set.contains(it) }
+    }
+
+    fun Char.toPriority() = if (this.isLowerCase()) this - 'a' + 1 else this - 'A' + 27
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf {
+            val firstHalf = it.substring(0, it.length / 2)
+            val secondHalf = it.substring(it.length / 2)
+
+            findCommon(firstHalf, secondHalf)
+                .first()
+                .toPriority()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .chunked(3)
+            .sumOf { group ->
+                group
+                    .reduce { acc, it -> findCommon(acc, it) }
+                    .first()
+                    .toPriority()
+            }
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
