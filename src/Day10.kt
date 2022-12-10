5f1f2ddc768481f9e57c13fb742a13e27e02a8d1
Hussein Jafferjee <hussein@jafferjee.ca>
2022-12-10 10:12:22 -0800

    day10 - implementation of part1 and part2

---
+++ src/Day10.kt
@@ -0,0 +1,55 @@
+fun main() {
+    fun buildSignal(input: List<String>): List<Int> {
+        return input.fold(listOf(1)) { acc, it ->
+            val register = acc.last()
+
+            when (it.take(4)) {
+                "noop" -> acc.plus(register)
+                "addx" -> acc.plus(listOf(register, register + it.substring(5).toInt()))
+                else -> acc
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val signal = buildSignal(input)
+
+        return (20..signal.size step 40).sumOf {
+            it * signal[it - 1]
+        }
+    }
+
+    fun part2(input: List<String>): String {
+        return buildSignal(input)
+            .mapIndexed { pixel, signal ->
+                val index = pixel % 40
+
+                if (index >= signal - 1 && index <= signal + 1) {
+                    "#"
+                } else {
+                    "."
+                }
+            }
+            .windowed(40, 40, false)
+            .joinToString("\n") {
+                it.joinToString("")
+            }
+    }
+
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 13140)
+    check(
+        part2(testInput) == """
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+    """.trimIndent()
+    )
+
+    val input = readInput("Day10")
+    println(part1(input)) // 14160
+    println(part2(input))
+}
