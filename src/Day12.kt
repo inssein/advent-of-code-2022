f8e11329da36b758d6366cca9e5ac83b34e5520f
Hussein Jafferjee <hussein@jafferjee.ca>
2022-12-12 18:56:01 -0800

    day12 - refactor to not need valid neighbour check

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,7 +1,7 @@
 import java.util.PriorityQueue
 
 data class Edge(val point: Pair<Int, Int>, val elevation: Int) : Comparable<Edge> {
-    override fun compareTo(other: Edge): Int = elevation.compareTo(other.elevation)
+    override fun compareTo(other: Edge) = elevation.compareTo(other.elevation)
 }
 
 data class Grid(val grid: List<List<Int>>) {
@@ -10,7 +10,6 @@ data class Grid(val grid: List<List<Int>>) {
 
     fun shortestPath(
         start: Pair<Int, Int>,
-        isValidNeighbour: (Int, Int) -> Boolean,
         isDestination: (Pair<Int, Int>) -> Boolean
     ): Int {
         val visited = mutableSetOf<Pair<Int, Int>>()
@@ -24,7 +23,7 @@ data class Grid(val grid: List<List<Int>>) {
                 continue
             }
 
-            val neighbours = neighbours(point, isValidNeighbour)
+            val neighbours = neighbours(point)
 
             if (neighbours.any { isDestination(it) }) {
                 return elevation + 1
@@ -36,7 +35,7 @@ data class Grid(val grid: List<List<Int>>) {
         error("No valid path.")
     }
 
-    private fun neighbours(point: Pair<Int, Int>, isValidNeighbour: (Int, Int) -> Boolean): List<Pair<Int, Int>> {
+    private fun neighbours(point: Pair<Int, Int>): List<Pair<Int, Int>> {
         val (x, y) = point
 
         return listOfNotNull(
@@ -44,7 +43,7 @@ data class Grid(val grid: List<List<Int>>) {
             if (y == height - 1) null else x to y + 1, // down
             if (x == 0) null else x - 1 to y, // left
             if (x == width - 1) null else x + 1 to y // right
-        ).filter { isValidNeighbour(value(point), value(it)) }
+        ).filter { value(point) - value(it) <= 1 }
     }
 
     fun value(point: Pair<Int, Int>) = grid[point.second][point.first]
@@ -73,18 +72,18 @@ fun main() {
     fun part1(input: List<String>): Int {
         val (grid, start, end) = input.parse()
 
-        return grid.shortestPath(start, { a, b -> b - a <= 1 }, { it == end })
+        return grid.shortestPath(end) { it == start }
     }
 
     fun part2(input: List<String>): Int {
         val (grid, _, end) = input.parse()
 
-        return grid.shortestPath(end, { a, b -> a - b <= 1 }, { grid.value(it) == 0 })
+        return grid.shortestPath(end) { grid.value(it) == 0 }
     }
 
     val testInput = readInput("Day12_test")
-    check(part1(testInput) == 31)
-    check(part2(testInput) == 29)
+    println(part1(testInput)) // 31
+    println(part2(testInput)) // 29
 
     val input = readInput("Day12")
     println(part1(input)) // 472
